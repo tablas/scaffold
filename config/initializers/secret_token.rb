93dcb7c6a3467f0902664f2fa2e8f29ddd1ae021
Andres Echeverri <andresecheverr@misena.edu.co>
2013-04-25 09:45:45 -0500

    proyecto rails sin scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tablas::Application.config.secret_token = '9078bed880124013a06f5eb9b0ce330135637991b1f83789071160b9499b7fb32cf815c8a05818bf6fe37649b222cd94b31d0990300c20b2c9b825a5d527a94f'
